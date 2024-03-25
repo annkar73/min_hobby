a1582b75b95c1c3828b7ff09864a42064e0f4c69
Anna Karlsen <146820987+annkar73@users.noreply.github.com>
2024-03-25 14:36:31 +0100

    :construction: added basic structure for contact

--- main.js
+++ main.js
@@ -19,7 +19,8 @@ function showAbout() {
   let aboutParagraph1 = document.createElement('p');
   aboutParagraph1.textContent = "Jag är en helt vanligt ovanlig person som gillar att samla på saker. Det handlar väl inte så mycket om att samla som att jag inte tycker om att slänga saker som det inte är fel på - och så finns det vissa saker som jag vill ha fler av.";
   let aboutParagraph2 = document.createElement('p');
-  aboutParagraph2.textContent = "När jag inte samlar mina skatter på hög ägnar jag mig åt att sortera dem, lägga dem på bra ställen där jag glömmer att jag har dem och ibland använder jag dem till och med - tex de där lådorna med garn som jag till och från flyttar på och sedan rotar runt i för att sticka bollar. "
+  aboutParagraph2.textContent = "När jag inte samlar mina skatter på hög ägnar jag mig åt att sortera dem, lägga dem på bra ställen där jag glömmer att jag har dem och ibland använder jag dem till och med - tex de där lådorna med garn som jag till och från flyttar på och sedan rotar runt i för att sticka bollar."
+
   contentDiv.appendChild(aboutTitle);
   aboutTitle.appendChild(aboutParagraph1);
   aboutTitle.appendChild(aboutParagraph2);
@@ -27,7 +28,18 @@ function showAbout() {
 }
 
 function showContact() {
-  document.getElementById("content").textContent = "Kontakta mig för mer information";
+  let contentDiv = document.getElementById('content');
+  contentDiv.textContent = "";
+  let contactTitle = document.createElement('h2');
+  contactTitle.textContent = "Kontakta mig:";
+  let contactParagraph1 = document.createElement('p');
+  contactParagraph1.textContent = "Via mail:";
+  let contactParagraph2 = document.createElement('p');
+  contactParagraph2.textContent = "Via LinkedIn:";
+
+  contentDiv.appendChild(contactTitle);
+  contactTitle.appendChild(contactParagraph1);
+  contactTitle.appendChild(contactParagraph2);
 }
 
 document.getElementById("homeLink").addEventListener("click", showHome);
