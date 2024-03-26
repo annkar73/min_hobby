10352e2e2ecc398ed6b1bfda3cd1b0ee329f9ffd
Anna Karlsen <146820987+annkar73@users.noreply.github.com>
2024-03-26 12:07:15 +0100

    initial array with recipes

---
+++ recipe.js
@@ -0,0 +1,36 @@
+const recipes = [
+    {
+        name: "Oskars frukostrutor",
+        ingredients: [
+            "500 g Semper Mix (spara ca 1 dl till utbakning)",
+            "3 dl fingervarmt vatten",
+            "50 g jäst",
+            "1 tsk salt",
+            "0,5 dl pofiber",
+            "1 dl Fibrex (finns numer endast malen, testa ev boveteflingor eller havregryn, eller uteslut)",
+            "1 dl neutral olja",
+            "ca 2 msk ljus sirap, kan bytas mot honung eller socker (eller uteslutes)",
+            "1,5 tsk Lindroos SoftBake",
+            "ca 1 dl riven ost",
+
+        ],
+        instructions: [
+            "Sätt ugnen på 230 grader (varmluftsugn 225 grader).",
+            "Lös upp jästen i vattnet med salt och ev sirap.",
+            "Tillsätt pofiber och ev Fibrex och låt det svälla i ca 15 minuter.",
+            "Tillsätt oljan och rör om.",
+            "Häll upp mjölet i en annan bunke och blanda ner Softbake. Ta inte all mjölmix direkt, glutenfri deg får gärna vara lite lös och kladdig.",
+            "Blanda ner i detvätskan och blanda till en lagom fast deg, lite kladdig men hanterbar.",
+            "Täck bunken med plastfolie (förhindrar att degen torkar) och gärna en handduk, och låt jäsa på dragfri plats i ca 30 minuter.",
+            "Strö lite av den återstående mjölmixen på en bänk, ta upp degen och knåda ihop lite och tryck ut till en rektangel.",
+            "Ta en ugnssäker form som är ca 20 x 30 cm och lägg i ett bakplåtspapper (om man skrynklar ihop det och slätar ut så är det enklare), strö lite mjölmix på pappret och lägg dit degen. Tryck ut så den täcker formen för att få jämna rutor.",
+            "Pensla med kallt vatten, strö på ost och ytterligare lite mjölmix.",
+            "Täck med plastfolie och handduk och låt jäsa ytterligare 30-45 minuter.",
+            "Grädda mitt i ugnen ca 15-20 minuter tills brödet har fått en gyllene färg och är genomgräddat.",
+        ],
+        notes: [
+            "Låt brödet svalna innan det skärs upp så är det mindre ris att det smular. Skär i rutor och frys in det som inte ska ätas de närmaste dagarna.",
+            "Om man är osäker på om brödet är färdiggräddat kan man lyfta ur det försiktigt och knacka i botten - det ska låta lite ihåligt.",
+        ]
+    },
+]
